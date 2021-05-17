130a5f2899dbfbcc8ae95ef052c886d45559e1e8
GitfishHub <318216007@qq.com>
2021-05-17 17:47:28 +0800

    引入antd

--- src/home.js
+++ src/home.js
@@ -1,14 +1,26 @@
 import React from 'react'
-
+import { Button, Icon, Divider } from 'antd'
+import {
+  SmileTwoTone,
+  HeartTwoTone,
+  CheckCircleTwoTone,
+} from '@ant-design/icons'
 export default class Home extends React.Component {
   render() {
     return (
-      <div
-        onClick={() => {
-          this.props.history.goBack()
-        }}
-      >
-        回到home
+      <div>
+        <Button
+          onClick={() => {
+            this.props.history.goBack()
+          }}
+          type="danger"
+        >
+          back
+        </Button>
+        <Divider dashed />
+        <SmileTwoTone />
+        <HeartTwoTone twoToneColor="#eb2f96" />
+        <CheckCircleTwoTone twoToneColor="#52c41a" />
       </div>
     )
   }
